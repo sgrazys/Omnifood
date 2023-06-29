5f27b98cde244d3cad51105ed2d188e450bd9a57
sgrazys <s.grazys@gmail.com>
2023-06-29 17:51:01 +0300

    compressed img

--- js/script.js
+++ js/script.js
@@ -52,7 +52,6 @@ const sectionHeroDOM = document.querySelector('.section-hero');
 const obs = new IntersectionObserver(
 	function (entries) {
 		const ent = entries[0];
-		console.log(ent);
 		if (!ent.isIntersecting) {
 			document.body.classList.add('sticky');
 		}
